68231e605de244ba66e0cce5f067f60137a5fec8
Juniorlynn-maker <khinelinntun8@gmail.com>
2025-03-09 19:47:01 +0000

    upload main.tf

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_iam_user" "junior-user" {
+  name = "junior-user" # Set the name of the IAM user
+}
+
+resource "aws_iam_user" "lynn-user" {
+  name = "lynn-user" # Set the name of the IAM user
+}
